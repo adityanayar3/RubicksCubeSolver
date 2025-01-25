d08a3c8052793718dc8921c8dc12b0a3118b598c
adityanayar3 <adinayar2003@gmail.com>
2025-01-26 00:47:58 +0530

    Initial Commit

---
+++ RubicksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ADITYA on 26-01-2025.
+//
+
+#include "RubicksCube.h"
---
+++ RubicksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by ADITYA on 26-01-2025.
+//
+
+#ifndef RUBICKSCUBE_H
+#define RUBICKSCUBE_H
+
+
+
+class RubicksCube {
+
+};
+
+
+
+#endif //RUBICKSCUBE_H
